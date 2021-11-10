4b40fb46860912763f336a051703c21083039dfa
nklingen <natasha.klingenbrunn@gmail.com>
2021-11-10 12:27:42 +0100

    captures characters

--- TheOffice.py
+++ TheOffice.py
@@ -1,29 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 baseurl = "https://theoffice.fandom.com/api.php?"
 action = "action=query"
 content = "prop=revisions&rvprop=content"
 dataformat ="format=json"
 
-page_titles = ["Main_Characters"]
+# characters are split across two pages
+page_titles = ["https://theoffice.fandom.com/wiki/Category:Characters", "https://theoffice.fandom.com/wiki/Category:Characters?from=Merv+Bronte"]
+categories = []
 
-#"Former_Employees", "Warehouse_worker"
+title = f"titles={page_title}"
+query = "{}{}&{}&{}&{}&rvslots=*".format(baseurl, action, content, title, dataformat)
 
 for page_title in page_titles:
+    page = requests.get(page_title)
 
-    title = f"titles={page_title}"
-    query = "{}{}&{}&{}&{}&rvslots=*".format(baseurl, action, content, title, dataformat)
+    soup = BeautifulSoup(page.content, "html.parser")
+    character_pattern = re.compile(r'/wiki/(.*)')
+    category_pattern = re.compile(r'Category:')
+    filter_subcategories = ["Background_Employees", "Clients_of_Dunder_Mifflin", "Main_Characters", "Mentioned_characters", "Voiced_Characters", "Background_Warehouse_Employees", "Dunder_Mifflin_family_members_and_loved_ones", "The_Office_Characters", "Angela%27s_cats"]
 
-    response = requests.get(query)
+    # for each of the characters listed in the characters pages
+    for link in soup.find_all("a", {"class": "category-page__member-link"}):
 
-    soup = BeautifulSoup(response.content, "html.parser")
+        # capture all the hrefs
+        href = link.get("href")
 
-    html_reponse = eval(str(soup))
+        # capture all characters in each href
+        name =  character_pattern.match(href).group(1)
 
-    page_id = list(html_reponse["query"]["pages"].keys())[0]
-    raw_text = html_reponse["query"]["pages"][page_id]["revisions"][0]['slots']['main']['*']
-    print(raw_text)
+        # remove "Categories" which are classifications of characters and not characters themselves
+        if category_pattern.match(name):
+            categories.append(name)
+            
+        # remove specific subcategories which are not characters themselves
+        elif name not in filter_subcategories:
+            print(name)
 
-    # with open(f'../files/zelda_characters/{character}.txt', 'w') as f:
+    # with open(f'characters/{character}.txt', 'w') as f:
     #     json.dump(json_text, f)
